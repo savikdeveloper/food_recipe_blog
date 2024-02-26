cd3c1aa089a954c43cfc80f022500d4fc3ba13e4
savikdev <savikdeveloper@gmail.com>
2024-02-26 17:23:33 +0500

    added dark and light mode

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+"use client";
+import React, { useContext } from 'react';
 import styles from "./themeToggle.module.css";
 import Image from 'next/image';
+import { ThemeContext } from '@/context/ThemeContext';
 
 const moonImg = require("/public/images/moon.png");
 const sunImg = require("/public/images/sun.png");
 
 const ThemeToggle = () => {
+  const {toggle, theme} = useContext(ThemeContext);
+  console.log(theme)
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src={moonImg} width={14} height={14} alt='moon'/>
       <div className={styles.ball}></div>
       <Image src={sunImg} width={14} height={14} alt='sun'/>
